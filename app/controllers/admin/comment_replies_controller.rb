de6c210c9d9ce57560b7adaa2c0d8f0f70259083
EC2 Default User <ec2-user@ip-172-31-32-173.ap-northeast-1.compute.internal>
2024-01-16 06:27:36 +0000

    controller route make

---
+++ app/controllers/admin/comment_replies_controller.rb
@@ -0,0 +1,2 @@
+class Admin::CommentRepliesController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,52 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :admis
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # 顧客用
+  # URL /customers/sign_in ...
+  devise_for :users, skip: [:passwords, :registrations], controllers: {
+    sessions: 'public/sessions'
+  }
+  
+  # フォロー関連
+  resources :follows, only: [:create, :destroy]
+
+  # ユーザー関連
+  resources :users, only: [:show, :edit, :update]
+
+  # いいね関連
+  resources :likes, only: [:create, :destroy]
+
+  # コメント関連
+  resources :comments, only: [:create, :destroy] do
+    resources :comment_replies, only: [:create, :destroy], shallow: true
+  end
+  
+  # 通知関連
+  resources :notifications, only: [:index, :destroy]
+
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admins, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
+  # フォロー関連
+  resources :follows, only: [:create, :destroy]
+
+  # ユーザー関連
+  resources :users, only: [:show, :edit, :update]
+
+  # いいね関連
+  resources :likes, only: [:create, :destroy]
+
+  # コメント関連
+  resources :comments, only: [:create, :destroy] do
+    resources :comment_replies, only: [:create, :destroy], shallow: true
+  end
+
+  # 通知関連
+  resources :notifications, only: [:index, :destroy]
+
+  # その他のルートも追加...
+
+  # public/registrationsに対するコントローラを生成する場合
+  get 'public/registrations', to: 'public/registrations#index'
 end

445d84c33910e621860acdf5a545225ada244214
EC2 Default User <ec2-user@ip-172-31-32-173.ap-northeast-1.compute.internal>
2024-03-16 16:27:12 +0000

    mig

--- db/migrate/20240316150444_create_likes.rb
+++ db/migrate/20240316150444_create_likes.rb
@@ -1,7 +1,8 @@
 class CreateLikes < ActiveRecord::Migration[6.1]
   def change
     create_table :likes do |t|
-
+      t.references :user, foreign_key: true
+      # 他のカラムの定義も追加する場合はここに記述
       t.timestamps
     end
   end

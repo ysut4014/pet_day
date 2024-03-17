1148a823bfa20829ebbfe888e6330dcd454740f9
EC2 Default User <ec2-user@ip-172-31-32-173.ap-northeast-1.compute.internal>
2024-03-17 05:01:11 +0000

    rename migration file2

--- db/migrate/20240316175129_create_relationships.rb
+++ db/migrate/20240316175129_create_relationships.rb
@@ -5,11 +5,10 @@ class CreateRelationships < ActiveRecord::Migration[6.1]
       t.bigint :followed_id
       t.datetime :created_at, precision: 6, null: false
       t.datetime :updated_at, precision: 6, null: false
-
+    end
       # follower_id が users テーブルの主キーを参照する外部キー制約を追加
       add_foreign_key :relationships, :users, column: :follower_id
       # followed_id が users テーブルの主キーを参照する外部キー制約を追加
       add_foreign_key :relationships, :users, column: :followed_id
-    end
   end
 end

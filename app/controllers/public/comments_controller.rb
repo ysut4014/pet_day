3327ed70e2433994cfbd8e8be0277e9599fe2602
EC2 Default User <ec2-user@ip-172-31-32-173.ap-northeast-1.compute.internal>
2024-04-11 18:19:46 +0000

    コメントcl

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,50 +1,56 @@
 class Public::CommentsController < ApplicationController
-def create
-  # フォームから送信されたコメントに関連する投稿を取得します
-  @post = Post.find(params[:comment][:post_id])
-  
-  # コメントを作成します
-  @comment = @post.comments.build(comment_params)
-  @comment.user = current_user
+  before_action :set_post, only: [:create]
+  before_action :set_comment, only: [:destroy]
 
-  # コメントが保存された場合の処理
-  if @comment.save
+  # コメントを投稿するアクション
+  def create
+    @comment = @post.comments.build(comment_params)
+    @comment.user = current_user
 
-    # 投稿の詳細ページを表示します
-    redirect_to post_path(@post)
-  else
-    # コメントが保存されなかった場合は、投稿の詳細ページを再表示します
-    redirect_to post_path(@post), alert: 'コメントの投稿に失敗しました。'
+    if @comment.save
+      redirect_to post_path(@post), notice: 'コメントを投稿しました。'
+    else
+      redirect_to post_path(@post), alert: 'コメントの投稿に失敗しました。'
+    end
   end
-end
 
+  # コメントを削除するアクション
+  def destroy
+    unless current_user_can_delete?
+      redirect_back fallback_location: root_path, alert: 'コメントを削除する権限がありません。'
+      return
+    end
 
-def destroy
-  @comment = Comment.find(params[:id])
-
-  # コメントに関連する通知も削除する
-  Notification.where(comment_id: @comment.id).destroy_all
-
-  # コメントの投稿者またはコメントの関連するポストの投稿者である場合にのみ削除を許可する
-  if @comment.user == current_user || @comment.post.user == current_user
     @comment.destroy
     redirect_back fallback_location: root_path, notice: 'コメントを削除しました。'
-  else
-    redirect_back fallback_location: root_path, alert: 'コメントを削除する権限がありません。'
   end
-end
 
-
-
+  # 全てのコメントを一覧表示するアクション
   def index
-    
     @comments = Comment.all
-    
-    
   end
+
   private
 
-def comment_params
-  params.require(:comment).permit(:post_id, :posted_text) 
-end
+  # 投稿を設定するメソッド
+  def set_post
+    @post = Post.find_by(id: params[:comment][:post_id])
+    redirect_back fallback_location: root_path, alert: '投稿が見つかりません。' unless @post
+  end
+
+  # コメントを設定するメソッド
+  def set_comment
+    @comment = Comment.find_by(id: params[:id])
+    redirect_back fallback_location: root_path, alert: 'コメントが見つかりません。' unless @comment
+  end
+
+  # コメントの投稿者または投稿者の投稿であるかを確認するメソッド
+  def current_user_can_delete?
+    @comment.user == current_user || @comment.post.user == current_user
+  end
+
+  # コメントのパラメーターを取得するメソッド
+  def comment_params
+    params.require(:comment).permit(:post_id, :posted_text)
+  end
 end

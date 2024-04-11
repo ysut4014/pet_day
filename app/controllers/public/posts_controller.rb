262017f861b615022706e598908e3c17f9b86530
EC2 Default User <ec2-user@ip-172-31-32-173.ap-northeast-1.compute.internal>
2024-04-11 18:29:04 +0000

    ポストcl

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,74 +1,71 @@
 class Public::PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_post, only: [:edit, :show, :update, :destroy]
+
+  # 新規投稿フォームを表示するアクション
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
-  # 新しい投稿を作成するためのアクション
+  # 新規投稿を作成するアクション
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.build(post_params)
     if @post.save
-      create_notifications_for_followers(@post) # 新しい投稿をフォロワーに通知
+      create_notifications_for_followers(@post) # フォロワーに通知を作成
       redirect_to posts_path
     else
       render 'new'
     end
   end
   
+  # 投稿の編集フォームを表示するアクション
   def edit
-    @post = Post.find(params[:id])
-    unless @post.user == current_user
-      redirect_to root_path, alert: "他のユーザーの投稿を編集する権限がありません。"
-    end    
-    
+    redirect_to root_path, alert: "他のユーザーの投稿を編集する権限がありません。" unless @post.user == current_user
   end  
 
-  # 特定の投稿を表示するためのアクション
+  # 特定の投稿を表示するアクション
   def show
-    @post = Post.find(params[:id])
-    @post.update(viewed: true) unless @post.viewed # 投稿が閲覧されたことを更新
+    @post.update(viewed: true) unless @post.viewed
     @comments = @post.comments.order(created_at: :desc).paginate(page: params[:page], per_page: 15)
-   if current_user
-     @reported_id = current_user.id
-     @reported_name = current_user.name
-   end
+    @reported_id = current_user.id if current_user # ログインユーザーのID
+    @reported_name = current_user.name if current_user # ログインユーザーの名前
   end    
 
-  # 投稿の一覧を表示するためのアクション
+  # 投稿の一覧を表示するアクション
   def index
-    if params[:search].present?
-      @posts = Post.where("title LIKE ? OR content LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%").order(created_at: :desc).paginate(page: params[:page], per_page: 10)
-    else
-      @posts = Post.all.order(created_at: :desc).paginate(page: params[:page], per_page: 10)
-    end
+    @posts = if params[:search].present?
+               Post.where("title LIKE ? OR content LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%")
+                   .order(created_at: :desc)
+                   .paginate(page: params[:page], per_page: 10)
+             else
+               Post.all.order(created_at: :desc)
+                   .paginate(page: params[:page], per_page: 10)
+             end
   end
 
-  # 投稿を削除するためのアクション
+  # 投稿を削除するアクション
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path
   end
   
+  # 投稿を更新するアクション
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to posts_path
     else
       render :edit
     end
-    
-    
   end  
   
-  # ユーザーをフォローするためのアクション
+  # ユーザーをフォローするアクション
   def follow
     @user = User.find(params[:id])
     current_user.followees << @user unless current_user.followees.include?(@user)
     redirect_to @user
   end
 
-  # ユーザーのフォローを解除するためのアクション
+  # フォローを解除するアクション
   def unfollow
     @user = User.find(params[:id])
     current_user.followees.delete(@user)
@@ -77,12 +74,17 @@ class Public::PostsController < ApplicationController
 
   private
 
-  # フォームから送信された投稿データを受け取るためのストロングパラメータ
+  # 特定の投稿をセットアップするメソッド
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  # 投稿パラメータを取得するストロングパラメータ
   def post_params
     params.require(:post).permit(:title, :content, :user_id, :image)
   end
 
-  # フォロワーに新しい投稿の通知を作成するためのメソッド
+  # フォロワーに新規投稿の通知を作成するメソッド
   def create_notifications_for_followers(post)
     followers = post.user.followers
     followers.each do |follower|
@@ -94,6 +96,4 @@ class Public::PostsController < ApplicationController
       )
     end
   end
-  
-  
 end
